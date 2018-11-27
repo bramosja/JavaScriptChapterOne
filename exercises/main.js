62a9349a9985a25df7a1deebca2376ead7d4c606
Brittany Ramos-Janeway <bramosjaneway@gmail.com>
2018-11-27 13:07:20 -0600

    fixed previous problems with code

--- exercises/main.js
+++ exercises/main.js
@@ -1,15 +1,23 @@
 // Use JavaScript to obtain a reference to the first section with the class of article_header and change its text with the textContent property to "Welcome to the {insert your name here} blog"
-const firstSection = document.querySelector(".article__header");
-firstSection.textContent = "Welcome to the Brittany blog"
+let firstSection = document.querySelector(".article__header");
+console.log(firstSection);
+
+firstSection.textContent = "Welcome to the Brittany blog";
 
 // Use JavaScript to obtain a reference to all article__header elements and change their classList property value to article__header important.
-const allSections = document.querySelectorAll (".article__header");
-allSections.classList.add("important");
+let allSections = document.querySelectorAll (".article__header");
+console.log("headers list", allSections)
+
+for (let i = 0; i < Headers.length; i++) {
+    headers[i].classList.add("important");
+}
+
+//For loop needs to be added
 
 // Obtain a reference the element with a class of dashed and remove it.
-const dashedSect = document.querySelector(".dashed");
-dashedSect.classList.remove(".dashed");
+let dashedSect = document.querySelector(".dashed");
+dashedSect.classList.remove("dashed");
 
 // Obtain a reference the element with a class of article_footer and add the class of goldenrod it.
-const artFoot = document.querySelector(".article__footer");
+let artFoot = document.querySelector(".article__footer");
 artFoot.classList.add("goldenrod");
